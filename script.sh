555d4f4cb61dfb873f2491fa34b3179e6a6d0112
SegunDapo <ladapsegun012@gmail.com>
2020-09-03 11:15:40 +0100

    MODULE-2

--- script.sh
+++ script.sh
@@ -1,2 +1,14 @@
 echo hello world
 
+echo This is my current working directory $PWD
+
+echo This is my bash $Bash
+
+echo This is my bash version $BASH_VERSION
+
+
+echo current path "$PATH"
+
+echo current prompt settings "$PS1"
+
+name='MR.Holistic'
